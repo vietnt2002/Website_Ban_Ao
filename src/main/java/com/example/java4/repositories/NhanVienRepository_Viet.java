724b73831485fe19b5bf67bb6b890e5496016489
vietnt2002 <nguyenviettt5@gmail.com>
2024-06-22 17:15:20 +0700

    Đăng nhập, đăng xuất, phân quyền & hiển thị tên và ảnh người đăng nhập

--- src/main/java/com/example/java4/repositories/NhanVienRepository_Viet.java
+++ src/main/java/com/example/java4/repositories/NhanVienRepository_Viet.java
@@ -1,6 +1,6 @@
 package com.example.java4.repositories;
 
-import com.example.java4.entities.NhanVien;
+import com.example.java4.entities.NhanVienViet;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,14 +10,14 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface NhanVienRepository
-        extends JpaRepository<NhanVien,String>
-{
+public interface NhanVienRepository_Viet extends JpaRepository<NhanVienViet, String> {
     public static final int ACTIVE  = 1;
     public static final int INACTIVE =0;
-    public Page<NhanVien> findByTrangThai(int trangThai, Pageable pageable);
-    public Optional<NhanVien> findById(String id);
+    public Page<NhanVienViet> findByTrangThai(int trangThai, Pageable pageable);
+    public Optional<NhanVienViet> findById(String id);
 
-    @Query("select nv from NhanVien nv where nv.taiKhoan = ?1")
-    NhanVien findByTaiKhoan(String tenTK);
-};
+    @Query("select nv from NhanVienViet nv where nv.taiKhoan = ?1")
+    NhanVienViet findByTaiKhoan(String tenTK);
+
+
+}
